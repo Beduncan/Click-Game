86fccd4f34c74358c7fb6a9bf228a6c5f4a7124f
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2018-03-29 12:43:01 -0700

    cleaned up css and added win lose options

--- src/App.js
+++ src/App.js
@@ -12,31 +12,65 @@ import Cars from './car.json';
 class App extends Component { 
   state = {
     Cars,
-    currentScore:0,
+    Score:0,
     topScore:0,
-    clicked:[]
+    message:"START",
+    clicked:[],
   };
   handleClick = id => {
     if(this.state.clicked.indexOf(id) === -1){
-      alert("you are right");
-
+      this.AddOne();
+      this.setState({
+        clicked: this.state.clicked.concat(id)
+      });
+      console.log(this.state.clicked);
     }else{
-      alert("you fucking suck");
-    }
+      this.setState({
+        message:"you lose"
+      });
+      // resets game
+      this.Loser();
+    };
+  };
+   //if they right add one to their score!
+  AddOne = () => {
+    const NewScore = this.state.Score + 1;
+    this.setState({
+      Score: NewScore,
+      message:"Your Right"
+    });
+    if (NewScore === 12) {
+      this.setState({message: "You Win!"});
+      //resets game
+      this.Loser
+      }//else if new score > topscore setState of topScore to newScore
+      // call a shuffle button 
    };
+   // run this when user loses it resets the game expect for the topScore
+   Loser = () =>{
+     this.setState({
+      message:"your wrong... Good Game",
+      clciked:[],
+      Score:0,
+     })
+   };
+   
+
+   // this is where you are rendering the html!
   render(){
   return(
-    <div>
-    <Header 
-    currentScore = {this.state.currentScore}
+    <div className="Header">
+    <Header
+    message = {this.state.message}
+    Score = {this.state.Score}
     />
     <Jumbotron />
     <div className="container" >
      {this.state.Cars.map(Cars => (
     <CBtn 
+      key= {Cars.id}
       handleClick={this.handleClick}
-      key ={Cars.id}
-      // Clicked={Cars.clicked}
+      id={Cars.id}
       image={Cars.img} 
       alt={Cars.alt}   
     />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,8 @@ const Header = props => {
 	return(
 		<div className="header">
 			<h1 className="title">Rick and Clicky</h1>
-			<p className="score"><span>Score: {props.currentScore}</span><br></br><span>High Score:</span></p>
+			<p className="message">{props.message}</p>
+			<p className="score"><span>Score: {props.Score}</span><br></br><span>High Score:</span></p>
 		</div>
 	);
 	
